12bcb641683a829d6070bac1c7d7075045801e16
nguyenn98 <nguyennguyenhvktqs98@gmail.com>
2025-07-13 22:12:01 +0700

    update complete logoUniversity

--- public/logos/logoUniversity.js
+++ public/logos/logoUniversity.js
@@ -298,6 +298,420 @@ const logoUniversity = [
         name: "Trường Đại học Hòa Bình",
         code: "HBU",
         logo: "/logos/dai_hoc_hoa_binh.png"
+    },
+    {
+        id: 51,
+        name: "Học viện Hành chính Quốc gia (trụ sở chính)",
+        code: "NAPA",
+        logo: "/logos/hoc_vien_hanh_chinh_quoc_gia.jpg"
+    },
+    {
+        id: 52,
+        name: "Học viện Chính trị khu vực I",
+        code: "HCPI",
+        logo: "/logos/hoc_vien_chinh_tri_khu_vuc_I.png"
+    },
+    {
+        id: 53,
+        name: "Trường Đại học Y Hà Nội (cơ sở phụ)",
+        code: "HMU2",
+        logo: "/logos/dai_hoc_Y_ha_noi(co_so_phu).png"
+    },
+    {
+        id: 54,
+        name: "Học viện Khoa học Xã hội",
+        code: "GASS",
+        logo: "/logos/hoc_vien_khoa_hoc_xa_hoi.png"
+    },
+    {
+        id: 55,
+        name: "Học viện Nông nghiệp Việt Nam",
+        code: "VNUA",
+        logo: "/logos/hoc_vien_nong_nghiep.png"
+    },
+    {
+        id: 56,
+        name: "Trường Đại học Văn hóa Nghệ thuật Quân đội",
+        code: "MCA",
+        logo: "/logos/dai_hoc_van_hoa_nghe_thuat_quan_doi.jpg"
+    },
+    {
+        id: 57,
+        name: "Học viện Phụ nữ Việt Nam",
+        code: "VWA",
+        logo: "/logos/hoc_vien_phu_nu_viet_nam.jpg"
+    },
+    {
+        id: 58,
+        name: "Học viện Báo chí và Tuyên truyền",
+        code: "AJC",
+        logo: "/logos/hoc_vien_bao_chi_va_tuyen_truyen.jpg"
+    },
+    {
+        id: 59,
+        name: "Đại học Quốc gia Hà Nội tại Hòa Lạc",
+        code: "VNU-HL",
+        logo: "/logos/ĐHQGHA_hoa_lac.png"
+    },
+    {
+        id: 60,
+        name: "Trường Đại học Lâm nghiệp",
+        code: "VNUF",
+        logo: "/logos/dai_hoc_lam_nghiep.jpg"
+    },
+    {
+        id: 61,
+        name: "Trường Đại học Chính trị",
+        code: "PAU",
+        logo: "/logos/dai_hoc_chinh_tri.jpg"
+    },
+    {
+        id: 62,
+        name: "Trường Đại học Thủ đô Hà Nội (cơ sở 3)",
+        code: "HNUE3",
+        logo: "/logos/Trường_Đại_học_Thủ_đô_Hà_Nội (co_so3).jpg"
+    },
+    {
+        id: 63,
+        name: "Học viện Chính trị",
+        code: "CPA",
+        logo: "/logos/hoc_vien_chinh_tri.jpg"
+    },
+    {
+        id: 64,
+        name: "Học viện Biên phòng",
+        code: "BPA",
+        logo: "/logos/hoc_vien_bien_phong.jpg"
+    },
+    {
+        id: 65,
+        name: "Trường Sĩ quan Pháo binh",
+        code: "AHA",
+        logo: "/logos/truong_si_quan_phao_binh.jpg"
+    },
+    {
+        id: 66,
+        name: "Trường Đại học Thủ đô Hà Nội (cơ sở 4)",
+        code: "HNUE4",
+        logo: "/logos/Trường_Đại_học_Thủ_đô_Hà_Nội (co_so 4).jpg"
+    },
+    {
+        id: 67,
+        name: "Trường Đại học Lao động - Xã hội",
+        code: "ULSA",
+        logo: "/logos/dai_hoc_lao_dong_xa_hoi.png"
+    },
+    {
+        id: 68,
+        name: "Học viện Hành chính Quốc gia (cơ sở 1)",
+        code: "NAPA1",
+        logo: "/logos/hoc_vien_hanh_chinh_quoc_gia(coso1).jpg"
+    },
+    {
+        id: 69,
+        name: "Trường Đại học Kinh tế Kỹ thuật Công nghiệp",
+        code: "UNETI",
+        logo: "/logos/truong_dai_hoc_kinh_te_ky_thuat_cong_nghiep.jpg"
+    },
+    {
+        id: 70,
+        name: "Trường Đại học Đại Nam",
+        code: "DNU",
+        logo: "/logos/dai_hoc_dai_nam.png"
+    },
+    {
+        id: 71,
+        name: "Trường Đại học Phenikaa",
+        code: "PHU",
+        logo: "/logos/dai_hoc_phenikaa.jpg"
+    },
+    {
+        id: 72,
+        name: "Trường Đại học Thủ đô Hà Nội (cơ sở 2)",
+        code: "HNUE2",
+        logo: "/logos/Trường_Đại_học_Thủ_đô_Hà_Nội (co_so2).jpg"
+    },
+    {
+        id: 73,
+        name: "Học viện Phật giáo Việt Nam tại Hà Nội",
+        code: "VBU",
+        logo: "/logos/hoc_vien_phat_giao_viet_nam_hn.jpg"
+    },
+    {
+        id: 74,
+        name: "Trường Đại học Công nghiệp Việt - Hung",
+        code: "VIHU",
+        logo: "/logos/dai_hoc_cong_nghiep_viet_hung.png"
+    },
+    {
+        id: 75,
+        name: "Học viện Chính sách và Phát triển",
+        code: "APD",
+        logo: "/logos/hoc_vien_chinh_sach_va_phat_trien.png"
+    },
+    {
+        id: 76,
+        name: "Đại học Quốc gia Hà Nội",
+        code: "VNU",
+        logo: "/logos/ĐHQGHA.png"
+    },
+    {
+        id: 77,
+        name: "Trường Đại học Công nghiệp Dệt may Hà Nội",
+        code: "HTU",
+        logo: "/logos/truong_dai_hoc_cong_nghiep_det_may_hn.png"
+    },
+    {
+        id: 78,
+        name: "Trường Đại học Kiểm sát Hà Nội",
+        code: "HUPL",
+        logo: "/logos/truong_dai_hoc_kiem_soat_hn.png"
+    },
+    {
+        id: 79,
+        name: "Học viện Tòa án",
+        code: "JAV",
+        logo: "/logos/hoc_vien_toa_an_vn.jpg"
+    },
+    {
+        id: 80,
+        name: "Học viện Quản lý Giáo dục",
+        code: "NIE",
+        logo: "/logos/hoc_vien_quan_ly_giao_duc.jpg"
+    },
+    {
+        id: 81,
+        name: "Học viện Y Dược học cổ truyền Việt Nam",
+        code: "VUTM",
+        logo: "/logos/hoc_vien_y_duoc_co_truyen_viet_nam.jpg"
+    },
+    {
+        id: 82,
+        name: "Trường Đại học Phương Đông (cơ sở 1)",
+        code: "PTU1",
+        logo: "/logos/dai_hoc_phuong_dong_cs1.jpg"
+    },
+    {
+        id: 83,
+        name: "Trường Đại học Công nghệ và Quản lý Hữu Nghị",
+        code: "UTM",
+        logo: "/logos/truong_dai hoc_cong_nghe_va_quan_ly_huu_nghi.png"
+    },
+    {
+        id: 84,
+        name: "Trường Đại học Kinh tế - Kỹ thuật Công nghiệp",
+        code: "UNETI",
+        logo: "/logos/truong_dai_hoc_kinh_te_ky_thuat_cong_nghiep.jpg"
+    },
+    {
+        id: 85,
+        name: "Trường Đại học Công nghiệp Hà Nội (cơ sở 2)",
+        code: "HAUI2",
+        logo: "/logos/dai_hoc_cong_nghiep_ha_noi_cs2.png"
+    },
+    {
+        id: 86,
+        name: "Trường Đại học Công nghiệp Việt-Hung (cơ sở Thạch Thất)",
+        code: "VIHU2",
+        logo: "/logos/dai_hoc_cong_nghiep_viet_hung (thach_that).png"
+    },
+    {
+        id: 87,
+        name: "Trường Đại học Điện lực (cơ sở 2)",
+        code: "EPU2",
+        logo: "/logos/dai_hoc_dien_luc_ha_noi_cs2.png"
+    },
+    {
+        id: 88,
+        name: "Trường Đại học Thủ đô Hà Nội",
+        code: "HNUE",
+        logo: "/logos/Trường_Đại_học_Thủ_đô_Hà_Nội.jpg"
+    },
+    {
+        id: 89,
+        name: "Trường Đại học Phương Đông (cơ sở 2)",
+        code: "PTU2",
+        logo: "/logos/dai_hoc_phuong_dong_cs2.jpg"
+    },
+    {
+        id: 90,
+        name: "Trường Cao đẳng Truyền hình",
+        code: "TVC",
+        logo: "/logos/truong_cao_dang_truyen_hinh.jpg"
+    },
+    {
+        id: 91,
+        name: "Trường Đại học Phòng cháy chữa cháy",
+        code: "PCCC",
+        logo: "/logos/dai_hoc_CSPCCC.png"
+    },
+    {
+        id: 92,
+        name: "Học viện Kỹ thuật Mật mã",
+        code: "MKU",
+        logo: "/logos/hoc_vien_ky_thuat_mat_ma.png"
+    },
+    {
+        id: 93,
+        name: "Học viện Hậu cần",
+        code: "HVHC",
+        logo: "/logos/hoc_vien_hau_can.jpg"
+    },
+    {
+        id: 94,
+        name: "Trường Đại học Kinh doanh và Công nghệ Hà Nội",
+        code: "HUBT",
+        logo: "/logos/dai_hoc_kinh_doanh_va_cong_nghe_hn.jpg"
+    },
+    {
+        id: 95,
+        name: "Trường Đại học Lao động - Xã hội (cơ sở Sơn Tây)",
+        code: "ULSA2",
+        logo: "/logos/dai_hoc_lao_dong_xa_hoi(son_tay).png"
+    },
+    {
+        id: 96,
+        name: "Trường Đại học Tài chính - Ngân hàng Hà Nội",
+        code: "FBU",
+        logo: "/logos/truong_dai_hoc_tai_chinh_ngan_hang_hn.png"
+    },
+    {
+        id: 97,
+        name: "Học viện Hành chính Quốc gia (cơ sở 2)",
+        code: "NAPA2",
+        logo: "/logos/hoc_vien_hanh_chinh_quoc_gia(coso2).jpg"
+    },
+    {
+        id: 98,
+        name: "Học viện Tài chính",
+        code: "AOF",
+        logo: "/logos/hoc_vien_tai_chinh.png"
+    },
+    {
+        id: 99,
+        name: "Trường Quản trị và Kinh doanh-ĐHQGHN",
+        code: "HSB",
+        logo: "/logos/truong_quan_tri_va_kinh_doanh_DHQGHN.png"
+    },
+    {
+        id: 100,
+        name: "Học viện Ngân hàng - Cơ sở đào tạo Sơn Tây",
+        code: "BAST",
+        logo: "/logos/hoc_vien_ngan_hang(cs_son_tay).png"
+    },
+    {
+        id: 101,
+        name: "Trường Đại học Mỏ - Địa chất (khu B)",
+        code: "HUMG_B",
+        logo: "/logos/truong_dai_hoc_mo_dia_chat_khu_B.jpg"
+    },
+    {
+        id: 102,
+        name: "Khoa Ngôn ngữ và Văn hóa Pháp, Đại học Ngoại ngữ - ĐHQGHN",
+        code: "ULIS-FR",
+        logo: "/logos/khoa_tieng_phap_DHNN_DHQGHN.jpg"
+    },
+    {
+        id: 103,
+        name: "Khoa Quốc tế – Đại học Quốc gia Hà Nội",
+        code: "VNU-IS",
+        logo: "/logos/khoa_quoc_te_DHQGHN.png"
+    },
+    {
+        id: 104,
+        name: "Đại học Quốc tế RMIT – Cơ sở Hà Nội",
+        code: "RMIT",
+        logo: "/logos/RMIT_University.jpg"
+    },
+    {
+        id: 105,
+        name: "Đại học Nguyễn Trãi",
+        code: "NTU",
+        logo: "/logos/dai_hoc_nguyen_trai.png"
+    },
+    {
+        id: 106,
+        name: "Trường Đại học Khoa học và Công nghệ Hà Nội",
+        code: "USTH",
+        logo: "/logos/dtruong_dai_hoc_khoa_hoc_va_cong_nghe_ha_noi.png"
+    },
+    {
+        id: 107,
+        name: "Học Viện Khoa Học Và Công Nghệ",
+        code: "GUST",
+        logo: "/logos/hoc_vien_khoa_hoc_va_cong_nghe.jpg"
+    },
+    {
+        id: 108,
+        name: "Học viện Dân tộc",
+        code: "VAEM",
+        logo: "/logos/hoc_vien_dan_toc.png"
+    },
+    {
+        id: 109,
+        name: "Trường Đại học Công nghệ Đông Á",
+        code: "EAUT",
+        logo: "/logos/dai_hoc_cong_nghe_dong_a.png"
+    },
+    {
+        id: 110,
+        name: "Trường Đại học Giáo dục - ĐHQGHN",
+        code: "UED",
+        logo: "/logos/truong_dai_hoc_giao_duc_DHQGHN.jpg"
+    },
+    {
+        id: 111,
+        name: "Trường Đại học CMC",
+        code: "CMC",
+        logo: "/logos/truong_dai_hoc_CMC.jpg"
+    },
+    {
+        id: 112,
+        name: "Trường Đại học Tài chính - Ngân hàng Hà Nội",
+        code: "FBU",
+        logo: "/logos/truong_dai_hoc_tai_chinh_ngan_hang_hn.png"
+    },
+    {
+        id: 113,
+        name: "Trường Đại học Kinh tế - ĐHQGHN",
+        code: "VNU-EUE",
+        logo: "/logos/truong_dai_hoc_kinh_te_DHQGHN.png"
+    },
+    {
+        id: 114,
+        name: "Đại học Luật - ĐHQGHN",
+        code: "VNU-UL",
+        logo: "/logos/truong_dai_hoc_luat_DHQGHN.png"
+    },
+    {
+        id: 115,
+        name: "Đại học Công nghệ - ĐHQGHN",
+        code: "UET",
+        logo: "/logos/truong_dai_hoc_cong_nghe_DHQGHN.png"
+    },
+    {
+        id: 116,
+        name: "Trường Đại học Y Dược - ĐHQGHN",
+        code: "UMP-VNU",
+        logo: "/logos/dai_hoc_y_duoc_DHQGHN.png"
+    },
+    {
+        id: 117,
+        name: "Trường Đại học Việt Nhật (VJU)",
+        code: "VJU",
+        logo: "/logos/dai_hoc_viet_nhat.png"
+    },
+    {
+        id: 118,
+        name: "Tòa E3 - Đại học Công Nghệ",
+        code: "UET-E3",
+        logo: "/logos/truong_dai_hoc_cong_nghe_DHQGHN.png"
+    },
+    {
+        id: 119,
+        name: "Học viện Công nghệ Bưu chính Viễn thông - Cơ sở đào tạo Ngọc Trục",
+        code: "PTIT-NT",
+        logo: "/logos/hoc_vien_cong_nghe_buu_chinh_vien_thong.png"
     }
 ];
 
